e9c0aa2db814bc96920b2f129dd8c4e62a455493
akash8864 <akash.yadav@technocodz.com>
2021-09-19 12:24:06 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,18 @@ const Navbar = () =>{
   return(  <nav className="bg-green-400">
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div className="relative flex items-center justify-between h-16">
-        
+        <Link href="/">
+            <a className="navbar-brand ">Note App</a>
+        </Link>
         
         {
             !loading && !session && (
                 
-               <div>
-                 <Link href="/">
-            <a className="navbar-brand ">Note App</a>
-        </Link> 
+                
             <Link href="/api/auth/signin">
             <a onClick={e=>{e.preventDefault()
             signIn()}}>sign In</a>
         </Link>
-        </div>
         )
         }
         {
